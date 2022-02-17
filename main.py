d92068b5d9e47299f914fd2399f7ffc0780d837c
mishchenko11 <maxim.y.mischenko@gmail.com>
2022-02-17 18:01:46 +0500

    com

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ app = Flask(__name__)
 
 
 @app.route('/<title>')
-@app.route('/index')
+@app.route('/index/<title>')
 def index(title):
+    return render_template('base.html', title=title)
+
+
+@app.route('/training/<speciality>')
+def training(speciality):
     param = {}
-    param['title'] = title
+    param['is_engineer'] = 'инженер' in speciality or 'строитель' in speciality
+
     return render_template('index.html', **param)
 
 
